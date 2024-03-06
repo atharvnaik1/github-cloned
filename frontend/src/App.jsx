073d82842ff1699a0d4986514c07caa4f855cd56
atharvnaik1 <atharv1599@gmail.com>
2024-03-06 12:44:45 +0530

    homepage=homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     <Sidebar/>
     <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1 '>
       <Routes>
-        <Route path="/" element={authUser ? <Homepage /> : <Navigate to={"/login"}/>}/>
+        <Route path="/" element={ <Homepage /> }/>
         <Route path="/login" element={!authUser ? <LoginPage/>:<Navigate to={"/"}/>}/>
         <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to={"/"}/>}/>
         <Route path="/explore" element={authUser ?<ExplorePage/> : <Navigate to={"/login"}/>}/>
