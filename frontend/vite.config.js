2861329b76d53c079d3570d0a477d52c7b26d618
atharvnaik1 <atharv1599@gmail.com>
2024-03-10 02:34:11 +0530

    api target change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:"https://github-clone-xjxq.onrender.com",
+        target:"http://localhost:5000",
       },
     }
   }
